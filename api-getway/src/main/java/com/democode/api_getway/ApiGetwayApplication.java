eee6707838e7333d71a387a3e15d79338a926745
hamarrass <h.hamarrass@gmail.com>
2025-01-22 11:24:52 +0100

    add config for api getway

--- api-getway/src/main/java/com/democode/api_getway/ApiGetwayApplication.java
+++ api-getway/src/main/java/com/democode/api_getway/ApiGetwayApplication.java
@@ -2,8 +2,10 @@ package com.democode.api_getway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGetwayApplication {
 
 	public static void main(String[] args) {
